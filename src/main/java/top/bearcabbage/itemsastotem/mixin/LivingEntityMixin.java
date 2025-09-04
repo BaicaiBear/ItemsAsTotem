2641599c3adfe58b7272ac11f56ea64526243b91
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-09-04 00:22:30 -0400

    v1.0.1 生命固定2.0，伤害吸收无效化（生命提升似乎也无效了）

--- src/main/java/top/bearcabbage/itemsastotem/mixin/LivingEntityMixin.java
+++ src/main/java/top/bearcabbage/itemsastotem/mixin/LivingEntityMixin.java
@@ -7,10 +7,13 @@ import net.minecraft.component.type.DeathProtectionComponent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
@@ -21,6 +24,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import top.bearcabbage.itemsastotem.DeathProtectorPayload;
 import top.bearcabbage.itemsastotem.RandomInventorySelector;
@@ -97,6 +101,13 @@ public abstract class LivingEntityMixin extends Entity {
 
                 cir.setReturnValue(deathProtectionComponent != null);
             }
+        }
+    }
+
+    @Inject(method = "setAbsorptionAmount", at = @At("HEAD"), cancellable = true)
+    private void setAbsorptionAmount(float absorptionAmount, CallbackInfo ci) {
+        if (this.isPlayer()){
+            ci.cancel();
         }
     }
 }
